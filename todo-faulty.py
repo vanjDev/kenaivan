6491317114a95d76bf17461cb1fc786f3d33507b
Zelle0919 <raizellechezka.munar@gmail.com>
2025-09-22 18:11:05 +0800

    [IMPROVEMENT]: Fixed formatting and UI issues

--- todo-faulty.py
+++ todo-faulty.py
@@ -1,44 +1,73 @@
-tasks=[]
+tasks = []
 
-def addtask(task):
+
+def add_task(task):
+    '''Adds a task to the task list.'''
     tasks.append(task)
-    print("task added!")
+    print("Task added!")
 
-def showTasks():
-    if len(tasks) == 0 :
-        print("no tasks yet")
+
+def show_tasks():
+    '''Displays all tasks in the task list.'''
+    if len(tasks) == 0:
+        print("No tasks yet")
     else:
+        print("\nTasks:")
         for i in range(len(tasks)):
             print(i + 1, ".", tasks[i])
 
-def removetask(tasknumber):
-    #fix off-by-one and invalid input
-    if tasknumber < 1 or tasknumber > len(tasks):
-        print("invalid task number!")
+
+def remove_task(task_number):
+    '''Removes a task from the task list by its number.'''
+    if task_number < 1 or task_number > len(tasks):
+        print("Invalid task number!")
         return
-    tasks.pop(tasknumber - 1)
-    print("task removed!!")
+    tasks.pop(task_number - 1)
+    print("Task removed!")
+
+
+def border():
+    '''Prints a border line.'''
+    print("=" * 21)
+
 
 def main():
+    '''Prints the header'''
+    border() 
+    print("TODO List Application")
+    border()
+
+    # Main loop for the TODO list application.
     while True:
-        print("1 Add Task")
-        print("2.Show Tasks")
-        print("3.Remove Task")
+        print("1. Add Task")
+        print("2. Show Tasks")
+        print("3. Remove Task")
         print("4. Exit")
-        ch = input("enter choice : ")
-        if ch== "1":
-            t = input("enter task : ")
-            addtask(t)
-        elif ch == "2":
-            showTasks()
-        elif ch == "3":
+
+        choice = input("\nEnter choice: ")
+
+        if choice == "1":
+            task = input("Enter task: ")
+            add_task(task)
+            border()
+        elif choice == "2":
+            show_tasks()
+            border()
+        elif choice == "3":
             try:
-                n = int(input("enter task no to remove: "))
-                removetask(n)
+                task_number = int(input("Enter task number to remove: "))
+                remove_task(task_number)
             except ValueError:
-                print("please enter a number only!")
-        elif ch == "4":
+                print("Please enter a number only!")
+            border()
+        elif choice == "4":
+            print("Exiting...")
+            border()
             break
         else:
-            print("wrong choice!!")
-main()
+            print("Wrong choice!")
+            border()
+
+
+if __name__ == "__main__":
+    main()
